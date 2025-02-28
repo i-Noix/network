80f5409c72b7cd169bbc9453837a067fd977c886
Roman Nazarov <rnazarov05@gmail.com>
2025-02-28 13:02:29 +0200

    Implement feature created_newPost in views, update urls and index.html, implement feature index so now  that show all posts

--- network/urls.py
+++ network/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("create_newPost", views.create_newPost, name="create_newPost"),
+
 ]
--- network/views.py
+++ network/views.py
@@ -1,14 +1,37 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect 
 from django.shortcuts import render
 from django.urls import reverse
+from django.views.decorators.http import require_POST
+from django.shortcuts import redirect
+from django.contrib import messages
 
-from .models import User
+from .models import User, Posts
 
 
 def index(request):
-    return render(request, "network/index.html")
+    posts = Posts.objects.all().order_by('-date_time')
+    return render(request, "network/index.html", {
+        'posts': posts
+    })
+
+@require_POST
+def create_newPost(request):
+    # Get post conent from submit form
+    content = request.POST.get('text', '').strip()
+    if content:
+         # Creating a new object to Posts
+        Posts.objects.create(
+            author = request.user,
+            content = content
+        )
+        messages.success(request, 'Post successfully created!')
+        # Redirect after create new post
+        return redirect("index")
+    else:
+        messages.info(request, "Content cannot be empty!")
+        return redirect("index")  
 
 
 def login_view(request):
